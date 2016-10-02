ac5653d15ea1f04e5c5be35a5e004458fdbcaa18
clever_man <clever.man.dev@gmail.com>
2016-10-02 22:57:40 +0300

    Initial commit

---
+++ app/src/main/java/com/bignerdbranch/android/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdbranch.android.nerdlauncher;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+public class NerdLauncherActivity extends SingleFragmentActivity {
+
+	@Override
+	protected Fragment createFragment() {
+		return NerdLauncherFragment.newInstance();
+	}
+}
